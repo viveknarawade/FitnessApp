a6081ade2e8bd9d934af953094be61894f9debfb
unknown <viveknarawade7264@gmail.com>
2024-10-29 21:39:04 +0530

    register started

--- FitLife/lib/Authentication/signUp.dart
+++ FitLife/lib/Authentication/signUp.dart
@@ -1,16 +1,16 @@
-import 'package:fitness_app/Authentication/signUp.dart';
+import 'package:fitness_app/Authentication/login.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class Login extends StatefulWidget {
-  const Login({super.key});
+class Signup extends StatefulWidget {
+  const Signup({super.key});
 
   @override
-  State<StatefulWidget> createState() => _LoginState();
+  State<Signup> createState() => _SignupState();
 }
 
-class _LoginState extends State<Login> {
+class _SignupState extends State<Signup> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,7 +26,7 @@ class _LoginState extends State<Login> {
             crossAxisAlignment: CrossAxisAlignment.stretch, // Ensure full width
             children: [
               Text(
-                "Hey there",
+                "Hey there,",
                 style: GoogleFonts.poppins(
                     fontSize: 17, fontWeight: FontWeight.w500),
               ),
@@ -37,8 +37,40 @@ class _LoginState extends State<Login> {
                     fontSize: 20, fontWeight: FontWeight.bold),
               ),
               const SizedBox(height: 20),
-              SvgPicture.asset("assets/authentication/loginScr.svg"),
-              const SizedBox(height: 45),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(13),
+                  ),
+                  hintText: "First Name",
+                  filled: true,
+                  fillColor: const Color.fromRGBO(247, 248, 248, 1),
+                  hintStyle: GoogleFonts.poppins(
+                      fontSize: 17, fontWeight: FontWeight.w300),
+                  prefixIcon: Padding(
+                    padding: const EdgeInsets.all(11),
+                    child: SvgPicture.asset("assets/icon/email.svg"),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(13),
+                  ),
+                  hintText: "Last Name",
+                  filled: true,
+                  fillColor: const Color.fromRGBO(247, 248, 248, 1),
+                  hintStyle: GoogleFonts.poppins(
+                      fontSize: 17, fontWeight: FontWeight.w300),
+                  prefixIcon: Padding(
+                    padding: const EdgeInsets.all(11),
+                    child: SvgPicture.asset("assets/icon/Lock.svg"),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
               TextField(
                 decoration: InputDecoration(
                   border: OutlineInputBorder(
@@ -51,7 +83,7 @@ class _LoginState extends State<Login> {
                       fontSize: 17, fontWeight: FontWeight.w300),
                   prefixIcon: Padding(
                     padding: const EdgeInsets.all(11),
-                    child: SvgPicture.asset("assets/icon/email.svg"),
+                    child: SvgPicture.asset("assets/icon/Lock.svg"),
                   ),
                 ),
               ),
@@ -84,7 +116,7 @@ class _LoginState extends State<Login> {
                   onPressed: () {
                     // Handle login logic here
                     Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => const Login()),
+                      MaterialPageRoute(builder: (context) => const Signup()),
                     );
                   },
                   child: Row(
@@ -97,7 +129,7 @@ class _LoginState extends State<Login> {
                       ),
                       const SizedBox(width: 10),
                       Text(
-                        "Login",
+                        "Register",
                         style: GoogleFonts.poppins(
                             fontSize: 20,
                             fontWeight: FontWeight.w500,
@@ -123,16 +155,18 @@ class _LoginState extends State<Login> {
                     width: 8,
                   ),
                   GestureDetector(
-                    onTap: (){
-                              Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => const Signup()),
-                    );
-                    },
+                      onTap: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                              builder: (context) => const Login()),
+                        );
+                      },
                       child: Text(
-                    "Register",
-                    style: GoogleFonts.poppins(
-                        fontSize: 17, color: Color.fromRGBO(150, 179, 254, 1)),
-                  )),
+                        "Login",
+                        style: GoogleFonts.poppins(
+                            fontSize: 17,
+                            color: Color.fromRGBO(150, 179, 254, 1)),
+                      )),
                 ],
               ),
             ],
