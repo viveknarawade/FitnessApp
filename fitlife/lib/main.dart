ce4c5b4e1b3c09b441f11515d26773ae4815b298
unknown <viveknarawade7264@gmail.com>
2024-11-07 18:23:07 +0530

    fitlife

--- fitlife/lib/main.dart
+++ fitlife/lib/main.dart
@@ -6,8 +6,11 @@ import 'package:flutter/material.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-
-  );
+      options: FirebaseOptions(
+          apiKey: "AIzaSyDz0jTJP_7pOA2VqTpKuK_R1zTOpVQajJI",
+          appId: "1:133562776760:android:d24731f903b36e82d7c84d",
+          messagingSenderId: "133562776760",
+          projectId: "fitnessapp-431bf"));
   runApp(const MainApp());
 }
 
